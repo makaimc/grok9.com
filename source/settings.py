7581a32ed773dd3270a1071a1b69ecce15e9590f
Matt Makai <matthew.makai@gmail.com>
2015-09-05 11:40:39 -0700

    starting to add settings in

---
+++ source/settings.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+AUTHOR = u'Matt Makai'
+SITENAME = u'Matt Makai'
+SITEURL = 'http://www.grok9.com'
+TIMEZONE = 'America/New_York'
+
+GITHUB_URL = 'https://github.com/makaimc/ngrok9.com'
+DISQUS_SITENAME = 'makaimc'
+PDF_GENERATOR = False
+
+DIRECT_TEMPLATES = ('index', 'sitemap', )
+
+SITEMAP_SAVE_AS = 'sitemap.xml'
+
+BYLINE = '&copy; 2015 Matt Makai. All Rights Reserved.'
+LINKS = ()
+
+MARKUP = ('rst', 'markdown',)
+
+SOCIAL = (
+    ('Email', 'mailto:makaimc@gmail.com'),
+    ('GitHub', 'https://github.com/makaimc'),
+    ('Twitter', 'http://twitter.com/mattmakai'),
+)
+
+PROJECTS = ()
+
+JINJA_EXTENSIONS = (['jinja2.ext.autoescape',])
+
